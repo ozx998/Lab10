27e9e342012b7a1a61b4faa24ab171dea2a8fea3
ozx998 <91934397+ozx998@users.noreply.github.com>
2021-11-25 12:05:08 +0800

    c

---
+++ Assets/Tests/TestSuite.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class TestSuite
+    {
+        private float result = 0.0f;
+
+        [Test]
+        public void TestAddition()
+        {
+            result = Calculator.CalculatePair(5, 2, "+");
+            Assert.AreEqual(result, 7);
+        }
+
+        [UnityTest]
+        public IEnumerable TestUnityAddition()
+        {
+            yield return null;
+
+            result = Calculator.CalculatePair(5, 2, "+");
+            Assert.AreEqual(result, 7);
+        }
+    }
+}
